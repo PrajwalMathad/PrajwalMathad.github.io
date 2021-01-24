f4ec8ddc922ada534c889f2bce2089da4b8b3e6c
Prajwal <prajwalmathad966@gmail.com>
2021-01-25 00:27:04 +0530

    initia commit

---
+++ src/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Contact = () => {
+    return (
+        <div className="contact-container">
+            <div className="phone">
+                Phone: 9663632966
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ src/home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="main-container">
+            <div id="myTopnav" class="navigation-bar">
+                <a href="#projects" >Projects</a>
+                <a href="#contact">Contact</a>
+            </div>
+            <div className="banner">
+                <div className="primary-text">
+                    Prajwal Mathad
+                </div>
+                <div className="secondary-text">
+                    Web UI Developer
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
