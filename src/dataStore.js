0691d5912d6ef2e6debaba5f87813eea8e92bb33
PrajwalMathad <prajwalmathad966@gmail.com>
2021-08-21 00:17:22 +0530

    Update

---
+++ src/dataStore.js
@@ -0,0 +1,49 @@
+const DataStore = {
+    experiences: [
+        {
+            type: "Job",
+            title: "Software Engineer",
+            company: "Software AG",
+            url: "https://www.softwareag.com/en_corporate.html",
+            place: "Bangalore",
+            dates: "Aug 2018 - Present",
+            description: [
+                "Building the next gen middleware cloud applications",
+                "Working on different platforms and frameworks like React, Angular, Node, JavaScript, Java, Docker, Kubernetes",
+                "Co-ordinating with multiple teams of engineers and designers",
+                "Writing efficient, readable, reusable, robust, modularized code",
+                "Used testing libraries like Jest, Protractor, React testing library"
+            ]
+        },
+        {
+            type: "Job",
+            title: "Research Assistant",
+            company: "KLE Technological University",
+            url: "https://www.kletech.ac.in/",
+            place: "Hubli",
+            dates: "Aug 2017 - Apr 2018",
+            description: [
+                "Built an end-to-end pipeline for 3D reconstruction of statues with images",
+                "Capture images of stautes with a drone and refine them",
+                "Worked on Image processing, Computer vision, Machine learning",
+                "Programming languages and Tools used : Python, C++, Matlab, VisualSFM, OpenMVG, OpenMVS",
+            ]
+        },
+        {
+            type: "Internship",
+            title: "Software Engineering Intern",
+            company: "Walmart Labs",
+            url: "https://corporate.walmart.com/",
+            place: "Bangalore",
+            dates: "Feb 2017 - Jul 2017",
+            description: [
+                "Worked with the global business solution steam of Walmart",
+                "Worked on SAP UI5, SAP ABAP, SAp Fiori to develop web applications"
+            ]
+        }
+    ],
+    ILove: [
+        "building websites", "sketching", "cars", "dogs", "technology", "being an engineer"
+    ]
+}
+export default DataStore;
--- src/home.js
+++ src/home.js
@@ -1,19 +1,59 @@
 import React from 'react';
+import DataStore from './dataStore';
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
+
+    const experienceDiv = () => {
+        return DataStore.experiences.map(exp => {
+            return (
+                <div className="single-exp">
+                    <div className="title">{exp.title}<a href={exp.url} className="company">&nbsp;@&nbsp;{exp.company}</a></div>
+                    <div className="date">{exp.dates}</div>
+                    <div className="exp-description">
+                        {exp.description.map(line => {
+                            return (
+                                <div className="single-point">
+                                    <i className="fas fa-angle-right"></i><div className="single-point-text">{line}</div>
+                                </div>)
+                        })}
+                    </div>
+                </div>
+            )
+        })
+    }
+
     return (
         <div className="main-container">
-            <div id="myTopnav" class="navigation-bar">
-                <a href="#/projects" >Projects</a>
-                <a href="#/contact">Contact</a>
+            <div className="left-container">
+                <div className="contacts">
+                    <a href="mailto:mathadprajwal@gmail.com" target="_blank" rel="noreferrer"><i class="icons fas fa-envelope"></i></a>
+                    <a href="https://www.linkedin.com/in/prajwal-mathad/" target="_blank" rel="noreferrer"><i class="icons fab fa-linkedin"></i></a>
+                    <a href="https://github.com/PrajwalMathad" target="_blank" rel="noreferrer"><i class="icons fab fa-github-square"></i></a>
+                    <a href="https://www.instagram.com/prajwal_mathad" target="_blank" rel="noreferrer"><i class="icons fab fa-instagram"></i></a>
+                </div>
             </div>
-            <div className="banner">
-                <div className="primary-text">
-                    Prajwal Mathad
+            <div className="middle-container">
+                <div className="banner">
+                    <div className="salutation"> Hey there! I am</div>
+                    <div className="primary-text">Prajwal Mathad</div>
+                    <div className="secondary-text" title="🤖: Its gonna take some time to typeout everything! This guy loves a lot of shit">
+                        <span>I love&nbsp;</span><Typewriter options={{ strings: DataStore.ILove, autoStart: true, loop: true }} />
+                    </div>
+                    <div className="intro">
+                        I'm a Software Engineer from India. Currently working at Software AG, building efficient and user friendly web applications.
+                        Thanks for stopping by. Lets catch up and talk tech!
+                    </div>
                 </div>
-                <div className="secondary-text">
-                    Web UI Developer!
+                <div className="secondary-container">
+                    <div className="exp-container section-container">
+                        <div className="section-header">Experience </div>
+                        {experienceDiv()}
+                    </div>
                 </div>
+                <div className="footer">built by Prajwal Mathad</div>
+            </div>
+            <div className="right-container">
             </div>
         </div>
     )
